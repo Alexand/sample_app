40e4999515b252fb9fd61122f55ebdf85f7a494d
JoaoAlexandre <j.a.cunha@gmail.com>
2014-09-17 15:53:36 -0300

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,6 +31,11 @@ describe User do
   	it { should_not be_valid }
   end
 
+  describe "when name is to short" do
+    before { @user.name = "aa" }
+    it { should_not be_valid }
+  end
+
   describe "when name is to long" do
   	before { @user.name = "a" * 51 }
   	it { should_not be_valid }
